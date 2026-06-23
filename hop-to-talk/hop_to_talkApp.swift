c43c46228f784a52cb7dabb513572b322db6fffd
adjiemuliadi <adjiem31@gmail.com>
2026-06-23 14:36:00 +0800

    Initial Commit

---
+++ hop-to-talk/hop_to_talkApp.swift
@@ -0,0 +1,17 @@
+//
+//  hop_to_talkApp.swift
+//  hop-to-talk
+//
+//  Created by Abuidillah Adjie Muliadi on 23/06/26.
+//
+
+import SwiftUI
+
+@main
+struct hop_to_talkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
